a5a9d918b8dda322e636d99dd4ffe5c09555ac7b
loiprocute <huuloi312001@gmail.com>
2023-08-04 17:34:11 +0700

    Add isotopic composition after depletion

---
+++ yolo_ver2.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ca698cdf65171150ee004b79249315dac2931019ec528cb3bd4839281a64b767
+size 6235
---
+++ yolov5-cpp.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:da835aafbaf511be484b9ff279da0f2ece660b3a4c65147e58e6b1e6d14ec985
+size 10508
