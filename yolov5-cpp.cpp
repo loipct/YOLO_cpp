e5a9b53b3df5a7fd8398e751c284c7264b4e77b1
loiprocute <huuloi312001@gmail.com>
2023-08-04 23:40:49 +0700

    add detections on image

--- yolov5-cpp.cpp
+++ yolov5-cpp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da835aafbaf511be484b9ff279da0f2ece660b3a4c65147e58e6b1e6d14ec985
-size 10508
+oid sha256:dc442b8b38ed171051ec8939d599e272ff34ff6fc5a55bd83f890f7ca638f73e
+size 9284
